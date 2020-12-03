724d9e3ecc6d0e30ad74a59aec30adddea28487a
waleedalmasri <67055177+waleedalmasri@users.noreply.github.com>
2020-12-03 12:32:11 +0200

    Initial commit

---
+++ app/src/main/java/com/example/classwork/MainActivity.java
@@ -0,0 +1,130 @@
+package com.example.classwork;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayAdapter<String> listAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        String url = "http://10.0.2.2:3306/rest/info.php";
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.INTERNET)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.INTERNET},
+                    123);
+
+        } else {
+            DownloadTextTask runner = new DownloadTextTask();
+            runner.execute(url);
+        }
+        listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        listAdapter.add("Books appears here");
+        ListView listView = (ListView) findViewById(R.id.listViewItems);
+        listView.setAdapter(listAdapter);
+    }
+
+
+    private InputStream OpenHttpConnection(String urlString) throws IOException {
+        InputStream in = null;
+        int response = -1;
+
+        URL url = new URL(urlString);
+        URLConnection conn = url.openConnection();
+
+        if (!(conn instanceof HttpURLConnection))
+            throw new IOException("Not an HTTP connection");
+        try {
+            HttpURLConnection httpConn = (HttpURLConnection) conn;
+            httpConn.setAllowUserInteraction(false);
+            httpConn.setInstanceFollowRedirects(true);
+            httpConn.setRequestMethod("GET");
+            System.out.println("here");
+            httpConn.connect();
+            response = httpConn.getResponseCode();
+            if (response == HttpURLConnection.HTTP_OK) {
+                in = httpConn.getInputStream();
+            }
+        } catch (Exception ex) {
+            Log.d("Networking", ex.getLocalizedMessage());
+            throw new IOException("Error connecting");
+        }
+        return in;
+    }
+
+    private String DownloadText(String URL) {
+        int BUFFER_SIZE = 2000;
+        InputStream in = null;
+        try {
+            in = OpenHttpConnection(URL);
+        } catch (IOException e) {
+            Log.d("Networking", e.getLocalizedMessage());
+            return "";
+        }
+
+        InputStreamReader isr = new InputStreamReader(in);
+        int charRead;
+        String str = "";
+        char[] inputBuffer = new char[BUFFER_SIZE];
+        try {
+            while ((charRead = isr.read(inputBuffer)) > 0) {
+                //---convert the chars to a String---
+                String readString =
+                        String.copyValueOf(inputBuffer, 0, charRead);
+                str += readString;
+                inputBuffer = new char[BUFFER_SIZE];
+            }
+            in.close();
+        } catch (IOException e) {
+            Log.d("Networking", e.getLocalizedMessage());
+            return "";
+        }
+        return str;
+    }
+
+
+    private class DownloadTextTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            return DownloadText(urls[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            String[] books = result.split(",");
+
+
+            for (int i = 0; i < books.length; i++) {
+                listAdapter.add(books[i]);
+            }
+
+
+        }
+    }
+}
